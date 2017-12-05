084ab09705c602ed67595ecc099230decf92a5fa
wenche <wenche.tollevsen@gmail.com>
2017-12-05 08:56:10 +0100

    fix: working pdf dialog

--- src/dialogs/media/Pdf.js
+++ src/dialogs/media/Pdf.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ImageFolderArchive from '../../media/ImageFolderArchive';
 
-const Pdf = () => {
-  const { onClick } = this.props;
+const Pdf = (props) => {
+  const { onClick } = props;
   return (
     <div>
       <ImageFolderArchive folderName="Pdf" isOpen isPdf onClick={onClick} />
     </div>
   );
+};
+
+Pdf.propTypes = {
+  onClick: PropTypes.func,
 };
 
 Pdf.defaultProps = {
