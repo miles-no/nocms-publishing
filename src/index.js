c1ecb3d90e9775411151f00a44182f28a0fd55eb
wenche <wenche.tollevsen@gmail.com>
2017-12-05 08:43:51 +0100

    feat: export editPdf

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ export { default as EditableComponent } from './EditableComponent';
 export { default as EditableComponentWrapper } from './EditableComponentWrapper';
 
 export { default as EditImage } from './media/EditImage';
+export { default as EditPdf } from './media/EditPdf';
 export { default as EditSimpleImage } from './media/EditSimpleImage';
 
 export { default as LinkButton } from './atoms/LinkButton';
