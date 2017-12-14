44c950705a399669e82247300c4a67f6fefb654f
wenche <wenche.tollevsen@gmail.com>
2017-12-14 21:44:21 +0100

    fix: somehow wrong language in image dialog

--- src/media/EditImage.js
+++ src/media/EditImage.js
@@ -24,7 +24,7 @@ export default class EditImage extends Component {
 
   render() {
     // @TODO: How should we represent instructions
-    const title = dictionary('Jeg ønsker å endre på bilde', this.context.lang);
+    const title = dictionary('Jeg ønsker å endre på bilde', 'no');
     return (
       <span> {this.props.activeEditMode && this.context.editMode ?
         <span className="admin-button__add-image">
