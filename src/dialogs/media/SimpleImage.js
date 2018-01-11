a31d2d1fa7df6ded30f2dc9f6848845e8de88301
wenche <wenche.tollevsen@gmail.com>
2018-01-11 12:34:48 +0100

    fix: remove check box for round images BREAKING CHANGE: no longer support round images due to buggy and way to interfering implementation

--- src/dialogs/media/SimpleImage.js
+++ src/dialogs/media/SimpleImage.js
@@ -5,7 +5,6 @@ import I, { dictionary } from '../../i18n/Internationalization';
 
 import Button from '../../atoms/Button';
 import ImageMeta from './ImageMeta';
-import Checkbox from '../../atoms/Checkbox';
 import ImageFolders from '../../media/ImageFolders';
 import cloudinary from '../../utils/cloudinary';
 
@@ -15,7 +14,6 @@ export default class SimpleImage extends Component {
     this.onFinish = this.onFinish.bind(this);
     this.onImageSelect = this.onImageSelect.bind(this);
     this.onMetaChange = this.onMetaChange.bind(this);
-    this.onCheckboxChange = this.onCheckboxChange.bind(this);
     this.state = {
       spinnerVisible: false,
       image: props.image,
@@ -42,44 +40,14 @@ export default class SimpleImage extends Component {
   }
 
   onImageSelect(info) {
-    if (this.state.image.isRound) {
-      const newImage = { publicId: info.publicId, format: info.format, isRound: true };
-      this.setState({ image: newImage });
-    } else {
-      const newImage = { publicId: info.publicId, format: info.format, isRound: false };
-      this.setState({ image: newImage });
-    }
-  }
-
-  onCheckboxChange(isChecked) {
-    if (this.state.image.publicId && isChecked) {
-      const updateObj = { isRound: true };
-      const imageObj = Object.assign({}, this.state.image, updateObj);
-      this.setState({ image: imageObj });
-    } else {
-      const updateObj = { isRound: false };
-      const imageObj = Object.assign({}, this.state.image, updateObj);
-      this.setState({ image: imageObj });
-    }
+    const newImage = { publicId: info.publicId, format: info.format };
+    this.setState({ image: newImage });
   }
 
   render() {
     const { config } = this.context;
-    let src;
-    if (this.state.image.publicId && this.state.image.isRound) {
-      const transformations = [
-        { quality: 'auto:eco',
-          radius: 'max',
-          gravity: 'face',
-          crop: 'fill',
-          border: '4px_solid_white',
-        },
-      ];
-      src = cloudinary.url(this.state.image.publicId, config.cloudinaryCloudName, { transformation: transformations });
-    } else {
-      const transformations = [{ quality: 'auto:eco' }];
-      src = cloudinary.url(this.state.image.publicId, config.cloudinaryCloudName, { transformations });
-    }
+    const transformations = [{ quality: 'auto:eco' }];
+    const src = cloudinary.url(this.state.image.publicId, config.cloudinaryCloudName, { transformations });
     const fileName = this.state.image.publicId ? `${this.state.image.publicId}.${this.state.image.format}` : 'Ingen valgt';
     return (
       <div>
@@ -97,10 +65,6 @@ export default class SimpleImage extends Component {
           </div>
           <div className="media__details">
             <ImageMeta onMetaChange={this.onMetaChange} {...this.state.image} />
-            {this.state.image.publicId ?
-              <Checkbox name="Gjør bilde rundt" label={dictionary('Gjør bildet rundt (*krever .png)', 'no')} onCheckboxChange={this.onCheckboxChange} />
-              : null
-            }
           </div>
         </div>
         <ImageFolders firstDefaultOpen activeImageId={this.state.image.publicId} />
